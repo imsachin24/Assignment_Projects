ad21dc0833c403c1b366fac756ad4633f59e1c48
Sachin Panigrahi <99538989+imsachin24@users.noreply.github.com>
2022-02-12 20:51:55 +0530

    Delete basic.c

--- basic.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-void main(){
-    printf("Name : Sachin Panigrahi\nRegistration Number : 048\nBranch : Electrical And Electronics Engineering\nHobbies : I love to play volleyball and watch cricket");
-}
